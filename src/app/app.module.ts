f1e1c7464c8d3b609dd05ed3dfceada51f8a46bf
Renan Bianchi <rnanbianchi@gmail.com>
2024-05-22 20:59:09 -0300

    implementing_autentication.v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import { AngularFireModule } from '@angular/fire/compat'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    AngularFireModule, AngularFireAuthModule, AngularFireModule.initializeApp(environment.firebaseConfig)],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideFirebaseApp(() => initializeApp({"projectId":"nostranonnapizza","appId":"1:685743795439:web:63182b3463f996a8813f1a","storageBucket":"nostranonnapizza.appspot.com","apiKey":"AIzaSyAlhV7TXNJqdWDdAcYHt6h89xMIu6ktRcs","authDomain":"nostranonnapizza.firebaseapp.com","messagingSenderId":"685743795439","measurementId":"G-ZZ3EYWC7K9"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/paginas/signup/signup.page.ts
+++ src/app/paginas/signup/signup.page.ts
@@ -1,4 +1,9 @@
+import { confirmPasswordReset } from '@angular/fire/auth';
+import { LoginPage } from './../login/login.page';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { LoadingController } from '@ionic/angular';
+import { AuthenticationService } from 'src/app/authentication.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +11,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
-
-  constructor() { }
+  regForm: FormGroup;
+  
+  constructor(public formBuilder:FormBuilder, public loadingCtrl: LoadingController, public authService:AuthenticationService) { }
 
   ngOnInit() {
+    this.regForm = this.formBuilder.group({
+      fullname: ['', [Validators.required]],
+      email: ['', [
+        Validators.required,
+        Validators.email,
+        Validators.pattern("[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$")  
+      ]],
+      address: ['', [Validators.required]],
+      password:['', [
+        Validators.required,
+        Validators.pattern("(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}")
+      ]
+      ],
+      confirmPassword: ['', [
+        Validators.required,
+        Validators.pattern("(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}")
+      ]
+      ]
+    })
+  }
+  get errorControl(){
+    return this.regForm?.controls;
+  }
+
+  async signUp(){
+    const loading = await this.loadingCtrl.create();
+    await loading.present();
+    if(this.regForm?.valid){
+      // const user = await this.authService.registerUser(email,password);
+    }
+
   }
 
 }
